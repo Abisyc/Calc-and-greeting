5c43c76abd9b3853d24fccc40b60cddd6e6d1463
Abisyc <abhishekdmoolya2005@gmail.com>
2025-07-18 11:50:30 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,7 @@
+#greeting
+a=input("Enter the first name")
+a=str(a)
+b=input("Enter the second name")
+b=str(b)
+add= a+b
+print(f'Welcome {add}! to the anaconda workshop')
---
+++ simple.py
@@ -0,0 +1,14 @@
+
+#Simple calculator
+a=input("enter the first number")
+a=float(a)
+b=input("enter the second number")
+b=float(b)
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition=",add)
+print("subtraction=",sub)
+print("multiplication=",mul)
+print("divisiom=",div)
